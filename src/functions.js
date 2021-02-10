ef70c633953fcb4ee709c7f5851a1cbd9701b14e
tong-lambda <tong.zhang@lambdaschool.com>
2021-02-10 11:42:15 -0800

    added functions for closure, recursion, nested

--- src/functions.js
+++ src/functions.js
@@ -13,11 +13,6 @@ export const calculateSquare = function (number) {
   return number * number;
 };
 
-//Recursive function
-export const factorial = function fac(n) {
-  return n < 2 ? 1 : n * fac(n - 1);
-};
-
 //Function that passes in as an argument
 export function map(f, a) {
   let result = [];
@@ -30,4 +25,95 @@ export function map(f, a) {
 
 export const f = function (x) {
   return x * x * x;
+};
+
+//Function scope
+let num1 = 1;
+let num2 = 2;
+const name = "globalName";
+
+export function canAccessGlobalVar() {
+  return num1 * num2;
+}
+
+//Recursive function
+export const factorial = function fac(n) {
+  return n < 2 ? 1 : n * fac(n - 1);
+};
+
+//Closure
+//Function scope local variables change global value
+export function getScore() {
+  num1 = 100;
+  num2 = 600;
+  function add() {
+    return name + " scored " + (num1 + num2);
+  }
+  return add();
+}
+
+export function addSquares(a, b) {
+  function square(x) {
+    return x * x;
+  }
+  return square(a) + square(b);
+}
+
+//Function inside can acess function outside's  arguments and variables
+//When calling outside function, x is preserved when inside is returned
+export function outside(x) {
+  function inside(y) {
+    return x / y;
+  }
+  return inside;
+}
+
+//Multiple-nested functions
+//B forms a closure including A (B can access A's arguments and variables)
+//C forms a closure including B (C can access both B and A's arguments and variables)
+export function A(x) {
+  function B(y) {
+    function C(z) {
+      return x + y + z;
+    }
+    return C;
+  }
+  return B;
+}
+
+//When there's naming conflicts, the inner-most scope takes the highest precedence
+export function namingConflict() {
+  let x = 5;
+  function inside(x) {
+    return x * 2;
+  }
+  return inside;
+}
+
+//Another closure example
+export let createPet = function (name) {
+  var sex;
+
+  return {
+    setName: function (newName) {
+      name = newName;
+    },
+
+    getName: function () {
+      return name;
+    },
+
+    getSex: function () {
+      return sex;
+    },
+
+    setSex: function (newSex) {
+      if (
+        typeof newSex === "string" &&
+        (newSex.toLowerCase() === "male" || newSex.toLowerCase() === "female")
+      ) {
+        sex = newSex;
+      }
+    },
+  };
 };
--- test/functions.spec.js
+++ test/functions.spec.js
@@ -5,6 +5,13 @@ import {
   factorial,
   map,
   f,
+  canAccessGlobalVar,
+  getScore,
+  addSquares,
+  outside,
+  A,
+  namingConflict,
+  createPet,
 } from "../src/functions.js";
 import assert from "assert";
 
@@ -26,15 +33,53 @@ describe("Test functions", function () {
   it("Should return the result of factorial function", function () {
     assert.strictEqual(factorial(3), 6);
   });
+
   it("Should return the result of map function", function () {
     const numbers = [-1, 0, 1, 2, 5];
     const result = map(f, numbers);
-    console.log(result);
     assert.strictEqual(result.length, 5);
     assert.strictEqual(result[0], -1);
     assert.strictEqual(result[1], 0);
     assert.strictEqual(result[2], 1);
     assert.strictEqual(result[3], 8);
     assert.strictEqual(result[4], 125);
+  });
+
+  it("Should return the result of canAccessGlobalVar function", function () {
+    assert.strictEqual(canAccessGlobalVar(), 2);
+  });
+
+  it("Should return the result of getScore function", function () {
+    assert.strictEqual(getScore(), "globalName scored 700");
+  });
+
+  it("Should return the result of addSquares function", function () {
+    assert.strictEqual(addSquares(3, 4), 25);
+  });
+
+  it("Should return the result of outside function", function () {
+    const fn_inside = outside(8);
+    const result1 = fn_inside(4);
+    assert.strictEqual(result1, 2);
+
+    const result2 = outside(10)(20);
+    assert.strictEqual(result2, 0.5);
+  });
+
+  it("Should return the result of A function", function () {
+    assert.strictEqual(A(3)(2)(1), 6);
+  });
+
+  it("Should return the result of namingConflict function", function () {
+    assert.strictEqual(namingConflict()(10), 20);
+  });
+
+  it("Should return the result of createPet function", function () {
+    let pet = createPet("John");
+    assert.strictEqual(pet.getName(), "John");
+    pet.setName("Oliver");
+    pet.setSex("female");
+    assert.strictEqual(pet.getName(), "Oliver");
+    assert.strictEqual(pet.getSex(), "female");
   });
 });
