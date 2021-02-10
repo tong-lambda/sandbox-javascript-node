6de4a18c89def69d88a10b6d11f3f0b5a818c650
tong-lambda <tong.zhang@lambdaschool.com>
2021-02-09 17:15:34 -0800

    fix export functions naming

--- src/functions.js
+++ src/functions.js
@@ -1,25 +1,25 @@
 // Named function
-function square(number) {
+export function square(number) {
   return number * number;
 }
 
 //Function that passes in an object
-function canTakeObject(theObject) {
+export function canTakeObject(theObject) {
   theObject.make = "Toyota";
 }
 
 //Assign function to variable and anonymus function
-const calculateSquare = function (number) {
+export const calculateSquare = function (number) {
   return number * number;
 };
 
 //Recursive function
-const factorial = function fac(n) {
+export const factorial = function fac(n) {
   return n < 2 ? 1 : n * fac(n - 1);
 };
 
 //Function that passes in as an argument
-function map(f, a) {
+export function map(f, a) {
   let result = [];
   let i;
   for (i = 0; i != a.length; i++) {
@@ -28,8 +28,6 @@ function map(f, a) {
   return result;
 }
 
-const f = function (x) {
+export const f = function (x) {
   return x * x * x;
 };
-
-export { square, canTakeObject, calculateSquare, factorial, map, f };
